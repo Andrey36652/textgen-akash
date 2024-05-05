ea90406e79030471f9621e1489e4c05cb30c9e63
Andrey36652 <35865938+Andrey36652@users.noreply.github.com>
2024-05-05 15:18:11 +0300

    Update install-and-run.sh

--- install-and-run.sh
+++ install-and-run.sh
@@ -1,8 +1,15 @@
 # начать с образа debian:12
 # находимся в домашней директории пользователя (/root)
 apt-get update && apt-get install wget net-tools openssh-server -y
+
+# Настроить доступ по SSH по публичному ключу
 mkdir -p ~/.ssh
 echo "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKwOV7xGnQ5Ft8zKGEGjS4kijY5PhzfoShWjUs+Negtz textgen-ssh-root-key-20240505" >> ~/.ssh/authorized_keys
 service ssh start
 echo "SSH server has started"
-sleep 86400
+
+# Загрузить koboldcpp
+wget -O koboldcpp https://github.com/LostRuins/koboldcpp/releases/download/v1.64.1/koboldcpp-linux-x64-cuda1150 && chmod +x koboldcpp
+
+# Спать
+sleep 172800
